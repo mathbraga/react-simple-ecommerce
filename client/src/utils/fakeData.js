9380643f24429fc5078ae5a92d53ab966eb7b29c
Matheus B <matheus_ob59@hotmail.com>
2022-06-23 21:36:18 -0300

    Create fake data for product card design.

---
+++ client/src/utils/fakeData.js
@@ -0,0 +1,22 @@
+const fakeData = [
+    {
+        "image": "imgUrl",
+        "brand": "apollo",
+        "name": "shirt",
+        "price": "50",
+    },
+    {
+        "image": "imgUrl",
+        "brand": "kine",
+        "name": "long sleeves",
+        "price": "60",
+    },
+    {
+        "image": "imgUrl",
+        "brand": "idadis",
+        "name": "sneakers",
+        "price": "80",
+    }
+];
+
+export default fakeData;
