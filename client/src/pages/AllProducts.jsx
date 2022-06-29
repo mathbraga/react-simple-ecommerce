97b91509d6d00c5884921e0ebea83bc915e06192
Matheus B <matheus_ob59@hotmail.com>
2022-06-28 23:20:34 -0300

    Update query with all data necessary.

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -18,12 +18,25 @@ class AllProducts extends PureComponent {
         });
     }
 
-    queryCategoriesNames = async () => {
-        const productFields = ['id', 'name', 'category'];
+    prepareQuery = () => {
+        const items = new Field('items', true).addField('value');
+        const attributeFields = [items, 'name', 'type'];
+        const attributes = new Field('attributes', true).addFieldList(attributeFields);
+        const currencyFields = ['label', 'symbol'];
+        const currency = new Field('currency', true).addFieldList(currencyFields);
+        const priceFields = [currency, 'amount'];
+        const prices = new Field('prices', true).addFieldList(priceFields);
+        const productFields = ['id', 'name', 'inStock', 'gallery', 'description', 
+        'category', attributes, prices, 'brand'];
+        const products = new Field('products', true).addFieldList(productFields);
         const query = new Query('category', true)
-            .addField(new Field('products', true)
-                .addFieldList(productFields)
-            );
+            .addField(products);
+
+        return query;
+    }
+
+    queryCategoriesNames = async () => {
+        const query = this.prepareQuery();
         const result = await client.post(query);
         this.handleState(result);
     }
@@ -42,6 +55,7 @@ class AllProducts extends PureComponent {
                     : 
                     null
                 }
+                {console.log(this.state.products['category'])}
             </PageContainer>
         );
     }
