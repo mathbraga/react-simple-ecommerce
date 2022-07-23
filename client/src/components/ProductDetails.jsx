f8e8d44dd09889d240367440d8997dda9598a7b9
Matheus B <matheus_ob59@hotmail.com>
2022-07-22 23:01:00 -0300

    Create cart button for product description.

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -4,6 +4,13 @@ import styled from "styled-components";
 const ProductDetailsStyles = styled.div`
     font-size: 1.875rem;
     font-weight: 400;
+    justify-self: stretch;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 20px;
 
     .product-brand{
         font-size: 1.875rem;
@@ -13,7 +20,20 @@ const ProductDetailsStyles = styled.div`
     .description{
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
-        font-size: 16px;
+        font-size: 1rem;
+    }
+
+    .btn-cart{
+        font-weight: 600;
+        color: white;
+
+        width: 100%;
+        background-color: var(--color-smooth-green);
+
+        border: none;
+        padding: 20px;
+
+        cursor: pointer;
     }
 `;
 
@@ -25,8 +45,11 @@ class ProductDetails extends PureComponent{
     render(){
         return(
             <ProductDetailsStyles>
-                <div className="product-brand">{this.props.brand}</div>
-                <div>{this.props.name}</div>
+                <div>
+                    <div className="product-brand">{this.props.brand}</div>
+                    <div>{this.props.name}</div>
+                </div>
+                <button className="btn-cart">ADD TO CART</button>
                 <div className="description" dangerouslySetInnerHTML={this.setHTML()}/>
             </ProductDetailsStyles>
         )
