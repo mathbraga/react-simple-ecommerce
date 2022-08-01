9f74b07a5f32f54c286ca64ad31607fd9f8972ff
Matheus B <matheus_ob59@hotmail.com>
2022-08-01 18:59:23 -0300

    Connect cart items redux store to cart component.

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,4 +1,5 @@
 import React, { PureComponent } from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
 import cartIcon from '../assets/icons/cart.svg';
 
@@ -14,12 +15,19 @@ const Styles = styled.div`
 
 class Cart extends PureComponent {
     render(){
+        const numberOfItems = this.props.cartItems.length;
+
         return(
             <Styles>
                 <img src={cartIcon} alt="Cart" />
+                <div>{numberOfItems}</div>
             </Styles>
         )
     }
 }
 
-export default Cart;
+const mapStateToProps = (state) => ({
+    cartItems: state.cart.cartItems
+})
+
+export default connect(mapStateToProps)(Cart);
