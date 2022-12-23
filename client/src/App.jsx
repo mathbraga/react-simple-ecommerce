9e95c5b4749f13312146f33b0623afc58691d76f
Matheus B <matheus_ob59@hotmail.com>
2022-12-22 23:41:06 -0300

    Remove unnecessary config file.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import React, { PureComponent } from "react";
 import { client } from "@tilework/opus";
 import PreDataLoader from "./PreDataLoader";
 
-client.setEndpoint(process.env.NODE_ENV === 'development' ? 'http://localhost:4000/' : process.env.API_ENDPOINT);
+client.setEndpoint(
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000/"
+    : "https://gql-dummy-server.vercel.app/api/graphql"
+);
 
 class App extends PureComponent {
-    render(){
-        return(
-            <PreDataLoader />
-        );
-    }
+  render() {
+    return <PreDataLoader />;
+  }
 }
 
 export default App;
